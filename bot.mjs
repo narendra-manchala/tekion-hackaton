b32db89fb68828025a00f2328e2a6bb6e393f9d3
anjaliSingh <anjalis@tekion.com>
2024-08-10 04:29:48 +0530

    sentry changes

--- bot.mjs
+++ bot.mjs
@@ -24,13 +24,43 @@ async function run(p, callback) {
  console.log(text);
  callback(text);
 }
+async function SentryRun(p, callback) {
+  const prompt = p || "Write a story about an AI and magic"
+  const promptQuestion = `
+  Convert the following data to the specified chart format without Explanation and remove Json from starting:
+  
+  Expected Format:
+  {
+    type: 'bar',
+    data: {
+      labels: ['TypeError', 'SyntaxError', 'NetworkError','Error: 403','Error: 500','Error: 404','Error: 401','Error: 504','UnknownError'],
+      datasets: [
+        {
+          label: 'Occurrences',
+          data: [405, 66, 292],
+        },
+      ],
+    },
+  }
+  `;
+  const result = await model.generateContent(`${promptQuestion}: ${prompt}`);
+  
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+  const encodedChart = encodeURIComponent(text);
+ const chartUrl = `https://quickchart.io/chart?c=${encodedChart}`; 
+  await callback(chartUrl);
+   callback('Fetching Explaination....');
 
-export default async function bot() {  
+  run(p, callback)
+ }
+
+export default async function bot(channelId) {  
   // const channelId = "C06AVC60TEU";
   // const userId = "U0688TRQG4E";
-  // const channelId = "C07DT9Y8754";
-  const channelId = "C06CDJDL97Y";
-  const userId =   "U0688SKTD2S";
+  // const channelId = "C06DF2QSBD1";
+  const userId =   "U06BW5ULQ6N";
 
   // call hugging face
   // const huggingfaceRes = await axios.post(apiUrl, payload, { headers });
@@ -72,7 +102,7 @@ export default async function bot() {
     const allMessages = await getChannelHistory(channelId);
     const userMessages = filterMessagesByUser(allMessages, userId);
 
-    if (userMessages.length === 0) {
+    if (userMessages.length === 0 && channelId !=='C06DF2QSBD1') {
       console.log("No messages found for user:", userId);
       return;
     }
@@ -91,7 +121,13 @@ export default async function bot() {
 
     const msg = formatMessagesWithLinks(userMessages);
     console.log("msg", msg);
-    await run(msg, postMessage);
+    if(channelId ==='C06DF2QSBD1') {
+     //  allMessages as errors are not user based 
+      const msg = formatMessagesWithLinks(allMessages);
+      await SentryRun(msg, postMessageSentry);
+      return msg; 
+    }
+  await run(msg, postMessage);
     return msg; 
     // TODO: Not working, no limit available
     // const completion = await openai.chat.completions.create({
@@ -127,7 +163,20 @@ export default async function bot() {
       console.error("Error posting message", error);
     }
   }
+  async function postMessageSentry (summary) {
+    try {
+      await app.client.chat.postMessage({
+        channel: channelId,
+        user: userId,
+        text: summary,
+      });
+      return "Posted data successfully.";
+    } catch (error) {
+      console.error("Error posting message", error);
+    }
+  }
 }
+
 
 let chat;
   // Message listener
@@ -138,7 +187,7 @@ slackApp.message('hello', async ({ message, say }) => {
     console.log('Called... ');
     console.log('-------------------');
     console.log(message);
-    context = await bot();
+    context = await bot('C07DT9Y8754');
     chat = model.startChat({
       history: [
         {
@@ -153,6 +202,19 @@ slackApp.message('hello', async ({ message, say }) => {
     // console.log('context', context);
     // console.log('------------->', result.response.text());
     // await say(`${result.response.text()}`);
+  } catch (e) {
+    console.log(e);
+  }
+});
+slackApp.message('Sentry analyze', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+ 
+  try {
+    console.log('Called... ');
+    console.log('-------------------');
+    console.log(message);
+    await say(`Analyzing Data...`);
+    context = await bot('C06DF2QSBD1');
   } catch (e) {
     console.log(e);
   }
